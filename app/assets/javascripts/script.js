e6809d5bfe5c603d2d0d598cf3f47f7a922b28dc
jktravis <joshua.travis@gmail.com>
2014-07-31 19:43:56 -0400

    Updated survey form and added some javascript.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,1 +1,24 @@
-$('#eventDate').datepicker();
+// Survey scripts
+$('#eventDate').datetimepicker({
+  timeFormat: 'HH:mm z',
+  timezoneList: [
+    { value: -300, label: 'Eastern'},
+    { value: -360, label: 'Central' },
+    { value: -420, label: 'Mountain' },
+    { value: -480, label: 'Pacific' }
+  ]
+});
+
+// show/hide meeting text field
+$('#meeting_type').change(function()
+{
+  if (this.value === 'Other')
+  {
+    $('#meeting_type_explaination').parent().parent().removeClass('hide');
+  }
+  else
+  {
+    $('#meeting_type_explaination').parent().parent().addClass('hide');
+    $('#meeting_type_explaination').val('');
+  }
+});
