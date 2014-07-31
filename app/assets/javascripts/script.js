d85fe00785be5b80086cc9b95a3c4a98ccd5558d
jktravis <joshua.travis@gmail.com>
2014-07-31 19:45:35 -0400

    Completed basic layout and functionality for the survey page itself. --skip-ci

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,16 +9,67 @@ $('#eventDate').datetimepicker({
   ]
 });
 
-// show/hide meeting text field
-$('#meeting_type').change(function()
+/*
+ * Helper function to make hiding/showing/clear more DRY
+ * 
+ * @param changeNode The jquery object node to watch for changes
+ * @param hideNode the jquery object node to hide
+ * @param checkVal the string value to check for (if true, hide)
+ * @param clearField the jquery object to clear the value of
+ */
+hideShowClear = function (changeNode, hideNode, checkVal, clearField)
 {
-  if (this.value === 'Other')
+  changeNode.change(function ()
   {
-    $('#meeting_type_explaination').parent().parent().removeClass('hide');
+    if (this.value === checkVal)
+    {
+      hideNode.removeClass('hide');
+    }
+    else
+    {
+      hideNode.addClass('hide');
+
+      if (clearField !== false) clearField.val('');
+    }
+  });
+};
+
+// show/hide meeting text field
+hideShowClear($('#meeting_type'), $('#meeting_type_explaination').parent().parent(),
+  'Other', $('#meeting_type_explaination') );
+
+// show/hide invocation-related fields
+hideShowClear($('#invocation_conducted'), $('.invocation_only'),
+  'Yes', false );
+
+// show/hide speaker preaching explanation
+hideShowClear($('#speaker_preached'), $('#preached_explain').parent().parent(),
+  'Yes', $('preached_explain') );
+
+// show/hide religion fields
+hideShowClear($('#religion'), $('#other_religion').parent().parent(),
+  'Other', $('#other_religion'));
+
+// show/hide denomination fields
+hideShowClear($('#denomination'), $('#other_denomination').parent().parent(),
+  'Other', $('#other_denomination'));
+
+// show/hide praised fields
+hideShowClear($('#speaker_praised'), $('#praised_explain').parent().parent(),
+  'Yes', $('#praised_explain'));
+
+// submit validation
+$('#submit').click(function(e)
+{
+  if ($('#policy:checked').val() !== 'on')
+  {
+    alert('Please review and agree to the policy before submission.');
+    e.preventDefault();
   }
   else
-  {
-    $('#meeting_type_explaination').parent().parent().addClass('hide');
-    $('#meeting_type_explaination').val('');
+  {// todo remove alert and preventDefaul() when the form can actually post
+    alert("Submitted! Well, not really. This is just a placeholder. ")
+    e.preventDefault();
   }
 });
+
