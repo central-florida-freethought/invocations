2a171b3f1a7df1134844b3595cad2d192f530fcb
Evan Machnic <emachnic@engineyard.com>
2014-08-11 14:02:35 -0400

    Added timeout for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :phone_number, presence: true
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+
   def set_default_role
     self.role ||= :user
   end
@@ -13,8 +18,7 @@ class User < ActiveRecord::Base
     self.update_attributes approved: true
   end
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  def timeout_in
+    30.minutes
+  end
 end
--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -3,6 +3,7 @@
 #   I want to sign in
 #   So I can visit protected areas of the site
 feature 'Sign in', :devise do
+  given(:user) { FactoryGirl.create :user }
 
   context 'user cannot sign in' do
 
@@ -21,7 +22,6 @@ feature 'Sign in', :devise do
     #   When I sign in with a wrong email
     #   Then I see an invalid email message
     scenario 'with wrong email' do
-      user = FactoryGirl.create :user
       signin 'invalid@email.com', user.password
       expect(page).to have_content 'Invalid email or password.'
     end
@@ -32,13 +32,17 @@ feature 'Sign in', :devise do
     #   When I sign in with a wrong password
     #   Then I see an invalid password message
     scenario 'with wrong password' do
-      user = FactoryGirl.create :user
       signin user.email, 'invalidpass'
       expect(page).to have_content 'Invalid email or password.'
     end
   end
 
   context 'user can sign in' do
+    given(:user) { FactoryGirl.create :user }
+
+    before do
+      user.confirm!
+    end
 
     # Scenario: User can sign in with valid credentials
     #   Given I exist as a user
@@ -46,21 +50,23 @@ feature 'Sign in', :devise do
     #   When I sign in with valid credentials
     #   Then I see a success message
     scenario 'with valid credentials but not approved' do
-      user = FactoryGirl.create :user
-      user.confirm!
       signin user.email, user.password
       expect(user.approved?).to be(false)
       expect(page).to have_content 'Signed in successfully.'
       expect(page).to have_content 'admin will review'
     end
 
-    scenario 'with valid credentials and approved' do
-      user = FactoryGirl.create :user
-      user.confirm!
-      user.approve!
-      signin user.email, user.password
-      expect(page).to have_content 'Signed in successfully'
-      expect(page).not_to have_content 'admin will review'
+    context 'with approval' do
+
+      before do
+        user.approve!
+      end
+
+      scenario 'and valid credentials' do
+        signin user.email, user.password
+        expect(page).to have_content 'Signed in successfully'
+        expect(page).not_to have_content 'admin will review'
+      end
     end
   end
 end
