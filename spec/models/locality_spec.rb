dc5f7f1be6be2ff7018c902bbbbe741f982ad094
Evan Machnic <emachnic@engineyard.com>
2014-08-11 01:13:59 -0400

    Cleaned up locality unit specs --skip-ci

--- spec/models/locality_spec.rb
+++ spec/models/locality_spec.rb
@@ -1,31 +1,21 @@
-require 'rails_helper'
+describe Locality do
+  it { is_expected.to belong_to(:contact) }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :street_address }
+  it { is_expected.to validate_presence_of :city }
+  it { is_expected.to validate_presence_of :state_code }
+  it { is_expected.to validate_presence_of :country_code }
+  it { is_expected.to validate_presence_of :zip }
+  it { is_expected.to validate_presence_of :volunteer_needed }
+  it { is_expected.to validate_presence_of :invocations_conducted }
+  it { is_expected.to validate_presence_of :contact }
 
-RSpec.describe Locality, :type => :model do
-  it { should belong_to(:contact) }
-
-  describe 'Success' do
+  context 'successfully' do
     it 'creates a new Locality given valid attributes' do
-      locality = FactoryGirl.build(:locality)
+      locality = FactoryGirl.build(:locality,
+        contact_attributes: FactoryGirl.attributes_for(:contact))
       expect(locality).to be_valid
       expect { locality.save }.to change(Locality, :count).by(1)
-    end
-  end
-
-  describe 'Failure' do
-    it "doesn't create a Locality without a contact name" do
-      locality = FactoryGirl.build :locality
-      locality.contact.name = ''
-      expect(locality).not_to be_valid
-    end
-
-    it "doesn't create a locality without a name" do
-      locality = FactoryGirl.build :locality, name: ''
-      expect(locality).not_to be_valid
-    end
-
-    it "doesn't create a locality without street address" do
-      locality = FactoryGirl.build :locality, street_address: ''
-      expect(locality).not_to be_valid
     end
   end
 end
