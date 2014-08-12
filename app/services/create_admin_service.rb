76f6322d38c84db0010ef89381808a21b4052bcd
jktravis <joshua.travis@gmail.com>
2014-08-12 17:26:37 -0400

    Corrected assignement order.

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -3,11 +3,11 @@ class CreateAdminService
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
-        user.confirm!
-        user.admin!
         user.first_name = Rails.application.secrets.admin_firstname
         user.last_name = Rails.application.secrets.admin_lastname
         user.phone_number = Rails.application.secrets.admin_phone
+        user.confirm!
+        user.admin!
       end
   end
 end
