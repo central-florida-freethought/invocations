bc6a2512d6cadbb5f59c89b03674e8b6735b6590
jktravis <joshua.travis@gmail.com>
2014-08-16 19:28:20 -0400

    Moved datetime js to its own file with the intention to only execute it on the page that needs it. --skip-ci

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,15 +1,3 @@
-// Survey scripts
-$('#user_meeting_meeting_time').datetimepicker({
-  dateFormat: 'D dd M yy',
-  timeFormat: 'HH:mm:ss z',
-  timezoneList: [
-    { value: -300, label: 'Eastern'},
-    { value: -360, label: 'Central' },
-    { value: -420, label: 'Mountain' },
-    { value: -480, label: 'Pacific' }
-  ]
-});
-
 /*
  * Helper function to make hiding/showing/clear more DRY
  * 
