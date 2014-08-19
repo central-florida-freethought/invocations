499e25d183b292148ddb98f389bce895e5852edc
jktravis <joshua.travis@gmail.com>
2014-08-19 15:27:04 -0400

    Deleted unused migration, which would fail.

--- db/migrate/20140818233501_add_explanations_to_user_meetings.rb
+++
@@ -1,6 +0,0 @@
-class AddExplanationsToUserMeetings < ActiveRecord::Migration
-  def change
-    add_column :user_meetings, :speaker_preached_explanation, :text
-    add_column :user_meetings, :speaker_praised_explanation, :text
-  end
-end
