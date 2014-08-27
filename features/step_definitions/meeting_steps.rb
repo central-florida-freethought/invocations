7288637ae2eefed4405f7c2584e0d016b1d08ea7
Evan Machnic <emachnic@engineyard.com>
2014-08-27 12:56:09 -0400

    More step definition refactorings

--- features/step_definitions/meeting_steps.rb
+++ features/step_definitions/meeting_steps.rb
@@ -1,8 +1,8 @@
 Given /^a (\w+)(?: with (\w+) "(.*?)")?$/ do |model, attribute, value|
   if attribute
-    @resource = Fabricate model, attribute => value
+    instance_variable_set "@#{model}", Fabricate(model, attribute => value)
   else
-    @resource = Fabricate model
+    instance_variable_set "@#{model}", Fabricate(model)
   end
 end
 
@@ -31,7 +31,7 @@ When /^I fill in the meeting fields$/ do
   fill_in 'Honorific', with: 'Mr.'
   fill_in 'Name of Speaker', with: 'Chunky Bacon'
   fill_in 'Organization or House of Worship', with: 'Hell House'
-  find('#user_meeting_speaker_attributes_religion_id').select @resource.name
+  find('#user_meeting_speaker_attributes_religion_id').select @religion.name
   fill_in 'Concerns', with: 'Bacon ipsum dolor sit amet cillum beef ribs biltong cow, chuck tongue prosciutto turkey aliqua tri-tip. Short loin aute frankfurter laboris qui tail. Ham hock voluptate t-bone shankle, rump minim chuck dolore.'
 end
 
