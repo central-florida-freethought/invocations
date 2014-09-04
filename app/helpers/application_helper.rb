478f33e0212fce7e4f4c747e9b8479aa998ec2da
jktravis <joshua.travis@gmail.com>
2014-09-04 15:29:54 -0400

    Updated sorting to include an icon --skip-ci.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,15 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, :sort => column, :direction => direction
+    css_class = nil
+    if column == sort_column
+      if direction == 'asc'
+        css_class = 'glyphicon glyphicon-sort-by-attributes-alt '
+      elsif direction == 'desc'
+        css_class = 'glyphicon glyphicon-sort-by-attributes '
+      end
+    end
+    content = link_to title, {:sort => column, :direction => direction}
+    content + ' ' + content_tag(:span, '', class: css_class)
   end
 end
