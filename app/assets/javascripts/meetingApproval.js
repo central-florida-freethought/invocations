d61c8e8fc8423d88b042401efc717e648739d63e
jktravis <joshua.travis@gmail.com>
2014-09-04 20:11:39 -0400

    Added some animation for the panel.  --skip-ci.

--- app/assets/javascripts/meetingApproval.js
+++ app/assets/javascripts/meetingApproval.js
@@ -1,6 +1,26 @@
 $('.toggleable').hide();
-$('.btn-primary').removeClass('hide').click(function(event){
+$('.slide-panel').slideUp();
+$('.btn-primary').removeClass('hide').click(function (event)
+{
+  var $this = $(this);
+  var mainTr = $this.closest('tr');
+  var hiding = $this.hasClass('hide-panel');
+
   event.preventDefault();
   event.stopPropagation();
-  $(this).closest('tr').next().toggle();
+
+  if (hiding)
+  {
+    mainTr.next().show().find('.slide-panel').slideDown();
+    $this.removeClass('hide-panel');
+  }
+
+  if (! hiding)
+  {
+    mainTr.next().find('.slide-panel').slideUp(function()
+    {
+      mainTr.next().hide();
+    });
+    $this.addClass('hide-panel');
+  }
 });
