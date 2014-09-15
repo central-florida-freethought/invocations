6bdc98028820ad1c34e01f55939d8a7cb83eb7c8
jktravis <joshua.travis@gmail.com>
2014-09-15 09:25:30 -0400

    Added helper methods for displaying links. --skip-ci.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_name(name)
-    content_for(:page_name) {name}
+    content_for(:page_name) { name }
   end
 
   def sortable(column, title = nil)
@@ -16,5 +16,23 @@ module ApplicationHelper
     end
     content = link_to title, {:sort => column, :direction => direction}
     content + ' ' + content_tag(:span, '', class: css_class)
+  end
+
+  # should probably be combined somehow with get_meeting_links
+  def get_obj_name(obj)
+    if obj.nil?
+      'Not provided'
+    else
+      obj.name
+    end
+  end
+
+  # should probably be combined somehow with get_obj_name
+  def get_meeting_links(link_text, obj)
+    if obj.nil?
+      link_text + ' not provided'
+    else
+      link_to link_text, obj, {:target => '_blank'}
+    end
   end
 end
