61337b5b66cb79e62e8f768d9517f469725db6c5
Evan Machnic <emachnic@engineyard.com>
2014-10-03 09:55:45 -0400

    Fixed style inconsistencies in controllers

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,5 +1,4 @@
 class ContactFormController < ApplicationController
-
   def new
     @message = Message.new
   end
@@ -9,11 +8,11 @@ class ContactFormController < ApplicationController
 
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => 'Message was successfully sent.')
+      redirect_to(root_path, notice: 'Message was successfully sent.')
     else
       flash.now.alert = 'Please fill all fields.'
       render :new
     end
   end
-
 end
+
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,34 +1,41 @@
 class RegistrationsController < Devise::RegistrationsController
-  
   def activate
     @user = User.find params[:id]
     if @user.update_attributes approved: true
       VolunteerMailer.approved_email(@user).deliver
-      redirect_to inactive_volunteers_path, notice: t('devise.registrations.activated')
+      redirect_to inactive_volunteers_path,
+                  notice: t('devise.registrations.activated')
     else
-      render inactive_volunteers_path, error: t('devise.registrations.activated_error')
+      render inactive_volunteers_path,
+             error: t('devise.registrations.activated_error')
     end
   end
-  
+
   def deactivate
     @user = User.find params[:id]
     if @user.update_attributes approved: false
       VolunteerMailer.deactivated_email(@user).deliver
-      redirect_to active_volunteers_path, notice: t('devise.registrations.deactivated')
+      redirect_to active_volunteers_path,
+                  notice: t('devise.registrations.deactivated')
     else
-      render active_volunteers_path, error: t('devise.registrations.deactiveated_error')
+      render active_volunteers_path,
+             error: t('devise.registrations.deactiveated_error')
     end
   end
 
   private
 
   def sign_up_params
-    params.require(:user).permit :first_name, :last_name, :email, :phone_number,
-      :password, :password_confirmation, :locality_id, :roles
+    params.require(:user)
+      .permit :first_name, :last_name, :email, :phone_number,
+              :password, :password_confirmation, :locality_id, :roles
   end
 
   def account_update_params
-    params.require(:user).permit :first_name, :last_name, :email, :phone_number,
-      :password, :password_confirmation, :locality_id, :current_password, :roles, :approved
+    params.require(:user)
+      .permit :first_name, :last_name, :email, :phone_number,
+              :password, :password_confirmation, :locality_id,
+              :current_password, :roles, :approved
   end
 end
+
