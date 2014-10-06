54b6346d72b2076f528e3f560ef793e6aab4a9af
Evan Machnic <emachnic@engineyard.com>
2014-10-06 11:24:12 -0400

    Fixed style issues for features dir

--- features/step_definitions/volunteer_steps.rb
+++ features/step_definitions/volunteer_steps.rb
@@ -1,13 +1,15 @@
-Given /^the volunteer is active$/ do
+Given(/^the volunteer is active$/) do
   @volunteer.approve!
 end
 
-When /^I (de)?activate the volunteer$/ do |prefix|
+When(/^I (de)?activate the volunteer$/) do |prefix|
   within '#confirmed_volunteers' do
     find("##{prefix}activate_volunteer_#{@volunteer.id}").click
   end
 end
 
-Then /^a(?:n)? (de)?activated email should be sent$/ do |prefix|
-  expect(ActionMailer::Base.deliveries.last.subject).to eq "Account #{prefix}activated"
+Then(/^a(?:n)? (de)?activated email should be sent$/) do |prefix|
+  last_subject = ActionMailer::Base.deliveries.last.subject
+  expect(last_subject).to eq "Account #{prefix}activated"
 end
+
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,25 +1,26 @@
 module NavigationHelpers
   def path_to(page_name)
     case page_name
-    
+
     when /the home page/
-      root_path   
+      root_path
     # Add more page name => path mappings here
     else
-      if path = match_rails_path_for(page_name) 
+      path = match_rails_path_for(page_name)
+      if path
         path
-      else 
-        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in features/support/paths.rb"
+      else
+        fail "Can't find mapping from \"#{page_name}\" to a path.\n" \
+        'Now, go and add a mapping in features/support/paths.rb'
       end
     end
   end
 
   def match_rails_path_for(page_name)
-    if page_name.match(/the (.*) page/)
-      return send "#{$1.gsub(" ", "_")}_path" rescue nil
-    end
+    return unless page_name.match(/the (.*) page/)
+    send "#{Regexp.last_match[1].gsub(' ', '_')}_path" rescue nil # rubocop:disable Style/RescueModifier, Metrics/LineLength
   end
 end
 
 World(NavigationHelpers)
+
