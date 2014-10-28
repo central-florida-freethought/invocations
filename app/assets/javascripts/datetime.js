435914ae0a23ae96978562ebb4befbf7a93c21d1
jktravis <joshua.travis@gmail.com>
2014-10-28 19:46:33 -0400

    Updated date formatting, which caused the date/time widget to not update correctly on edit. This was identified in Issue #38.

--- app/assets/javascripts/datetime.js
+++ app/assets/javascripts/datetime.js
@@ -1,11 +1,20 @@
-// Survey scripts
-$('#user_meeting_meeting_time').datetimepicker({
-  dateFormat: 'D dd M yy',
-  timeFormat: 'HH:mm:ss z',
-  timezoneList: [
-    { value: -300, label: 'Eastern'},
-    { value: -360, label: 'Central' },
-    { value: -420, label: 'Mountain' },
-    { value: -480, label: 'Pacific' }
-  ]
-});
+(function($)
+{
+  var currentDate = '';
+  var dateField = $('#user_meeting_meeting_time');
+  if (dateField.val() != '')
+  {
+    currentDate = dateField.val();
+  }
+  dateField.datetimepicker({
+    dateFormat: 'yy-mm-dd',
+    timeFormat: 'HH:mm:ss z',
+    defaultValue: currentDate,
+    timezoneList: [
+      { value: -300, label: 'Eastern'},
+      { value: -360, label: 'Central' },
+      { value: -420, label: 'Mountain' },
+      { value: -480, label: 'Pacific' }
+    ]
+  });
+})(jQuery);
