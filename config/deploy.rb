5c1cf3171216043daced9c09b898ffdf0b1e33ad
Evan Machnic <emachnic@engineyard.com>
2014-11-21 11:30:31 -0500

    Cap options for Digital Ocean --skip-ci

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,17 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'invocations'
+set :repo_url, 'git@bitbucket.org:cffc/invocations_rails.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/deploy/rails_apps/invocations'
 # set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle
-# public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
