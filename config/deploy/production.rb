334febfeb3083b2aa0b9f1f2fa76e83665af602f
Evan Machnic <emachnic@engineyard.com>
2014-11-21 16:36:19 -0500

    Updated cap config --skip-ci

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w(deploy@104.131.180.69)
+role :web, %w(deploy@104.131.180.69)
 #role :db,  %w(deploy@104.131.186.56)
 
 # Extended Server Syntax
