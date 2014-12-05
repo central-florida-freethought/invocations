9aedd5a544d1c9603eeeeeab40532c510deb8205
Evan Machnic <emachnic@engineyard.com>
2014-12-04 23:40:53 -0500

    Cleaned up deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,6 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 require 'mina/puma'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
 
 set :user, 'deploy'
 set :domain, '104.131.180.69'
@@ -17,19 +9,9 @@ set :deploy_to, '/home/deploy/rails_apps/invocations'
 set :repository, 'git@bitbucket.org:cffc/invocations_rails.git'
 set :branch, 'digitalocean'
 
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
-
-# Manually create these paths in shared/ (eg: shared/config/database.yml)
-# in your server. They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', '.env', 'log', 'bin', 'tmp/pids',
                     'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system'
                    ]
-
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
