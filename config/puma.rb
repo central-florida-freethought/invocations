3b07bf0675e1ee0bf47666dfb109b42d103a1da6
Evan Machnic <emachnic@engineyard.com>
2014-12-10 17:40:34 -0500

    Added docker config --skip-ci

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,12 @@
 #!/usr/bin/env puma
 
-directory '/home/deploy/rails_apps/invocations/current'
-rackup "/home/deploy/rails_apps/invocations/current/config.ru"
+directory '/usr/src/app'
+rackup "/usr/src/app/config.ru"
 environment 'production'
 
-pidfile "/home/deploy/rails_apps/invocations/shared/tmp/pids/puma.pid"
-state_path "/home/deploy/rails_apps/invocations/shared/tmp/pids/puma.state"
-stdout_redirect '/home/deploy/rails_apps/invocations/shared/log/puma_access.log', '/home/deploy/rails_apps/invocations/shared/log/puma_error.log', true
+pidfile "/usr/src/app/tmp/pids/puma.pid"
+state_path "/usr/src/app/tmp/pids/puma.state"
+stdout_redirect '/usr/src/app/log/puma_access.log', '/usr/src/app/log/puma_error.log', true
 
 threads 0,16
-
-bind 'unix:///home/deploy/rails_apps/invocations/shared/tmp/sockets/puma.sock'
-
-preload_app!
 
